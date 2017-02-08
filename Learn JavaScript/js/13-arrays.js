6021dbcca7505233f696ac3cd49a52e6a9a6406b
MichalSwiader <michal@swiader.pl>
2017-02-08 20:57:13 +0100

    14-vacation

---
+++ Learn JavaScript/js/13-arrays.js
@@ -0,0 +1,13 @@
+var myPlaces = ["Paris","Barcelona","Iceland"];
+var paulinaPlaces = ["Gdansk","Norway","Scotland"];
+
+for (var i = 0; i < myPlaces.length; i++) {
+	for (var j = 0; j < paulinaPlaces.length; j++) {
+		if (myPlaces[i] === paulinaPlaces[j]) {
+			console.log('We should go together to: ' + myPlaces[i])}
+		else (myPlaces[i] !== paulinaPlaces[j]) {
+			console.log('We should BREAK UP!!!')
+		}			
+		}
+	}
+}
